4899687ff4b6f38c6dd2c330a5e49227373b2f56
Josiah Johnson <josiah.johnson@ascte.org>
2025-08-29 09:10:27 -0500

    Added View Tasks

---
+++ todo.py
@@ -0,0 +1,38 @@
+# todo.py
+
+# Step 1: Start with an empty list to hold tasks
+tasks = []
+
+# Step 2: Add a task
+def add_task(task):
+    tasks.append(task)
+
+# Step 3: View tasks
+def view_tasks(task):
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+    
+
+# Step 4: Delete a task
+#def delete_task(index):
+    
+
+
+# Step 5: Mark task complete
+#def mark_complete(index):
+    
+
+
+# Step 6: Save/load tasks (extra stretch for today)
+#def save_tasks(filename="tasks.txt"):
+    
+
+
+# Demo flow (you can run this file directly: python todo.py)
+if __name__ == "__todo__":
+    add_task("Finish Cyber 201 assignment")
+    add_task("Push code to GitHub")
+    view_tasks()
+   # mark_complete(0)
+   # view_tasks()
+   # save_tasks()
